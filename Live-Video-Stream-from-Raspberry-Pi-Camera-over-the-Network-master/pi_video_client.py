033d4f1557bd7ed86b30d607717b9b268b4410d6
J <jamesqian1999@gmail.com>
2021-07-20 19:28:03 +0800

    smooth

--- Live-Video-Stream-from-Raspberry-Pi-Camera-over-the-Network-master/pi_video_client.py
+++ Live-Video-Stream-from-Raspberry-Pi-Camera-over-the-Network-master/pi_video_client.py
@@ -25,7 +25,7 @@ finally:
 
 '''
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(("192.168.0.178",10001))
+client_socket.connect(("192.168.0.178", 10001))
 
 connection = client_socket.makefile('wb')
 count = 1
@@ -36,15 +36,27 @@ try:
         print("starting Camera...........")
         time.sleep(2)
         stream = io.BytesIO()
-        for foo in camera.capture_continuous(stream, 'jpeg'):
+        count = 0
+        loop = time.time_ns()
+        cap = camera.capture_continuous(stream, 'jpeg', use_video_port = True)
+        print(type(cap))
+        for foo in cap:
+            print("\nloop\t", (time.time_ns()-loop)/10**9)
+
             connection.write(struct.pack('<L', stream.tell()))
             connection.flush()
+
+            a = time.time_ns()
             stream.seek(0)
             connection.write(stream.read())
+            print("a\t", (time.time_ns()-a)/10**9)
+
+            b = time.time_ns()
             stream.seek(0)
             stream.truncate()
+            print("b\t", (time.time_ns()-b)/10**9)
+
+            loop = time.time_ns()
 except:
-    #connection.close()
+    # connection.close()
     client_socket.close()
-
-
